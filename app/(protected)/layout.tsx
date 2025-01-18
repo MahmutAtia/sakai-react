6d5163e6428f746d62393c42ff00eee1973e24f3
Mahmut Atia <mahmoud.atia@turboard.com>
2025-01-18 09:49:05 +0300

    Feat: editor last desgin

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function SimpleLayout({ children }: SimpleLayoutProps) {
     return (
         <React.Fragment>
-            <AppBar />
+            {/* <AppBar /> */}
 
             {children}
             <AppConfig simple />
