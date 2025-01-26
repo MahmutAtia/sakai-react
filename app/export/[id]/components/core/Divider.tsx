8d3b835a41f157fcb458a343b9e19167f4919b68
Mahmut Atia <mahmoud.atia@turboard.com>
2025-01-26 21:56:13 +0300

    Fix: deleting latex

--- app/export/[id]/components/core/Divider.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { colors } from '../../theme'
-
-export const Divider = styled.hr`
-  margin: 24px 0;
-  background: ${colors.primary};
-  height: 4px;
-  border: none;
-`
